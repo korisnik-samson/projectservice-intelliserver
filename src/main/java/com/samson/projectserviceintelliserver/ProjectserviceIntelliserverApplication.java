9dcc614e98fd59d52e1596e6a2b67f8b8902fef3
Samson <126519078+korisnik-samson@users.noreply.github.com>
2025-02-11 16:15:48 +0100

    Initial Commit

---
+++ src/main/java/com/samson/projectserviceintelliserver/ProjectserviceIntelliserverApplication.java
@@ -0,0 +1,13 @@
+package com.samson.projectserviceintelliserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectserviceIntelliserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectserviceIntelliserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samson/projectserviceintelliserver/ProjectserviceIntelliserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samson.projectserviceintelliserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectserviceIntelliserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
